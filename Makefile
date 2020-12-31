b38d1facc7fec7e57e14fb4b78ec361bf5b2bc26
Hoernchen20 <felix@horn-witten.de>
2020-12-31 18:33:53 +0100

    refactor code

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 APPLICATION = ho_weather
 
 # If no BOARD is found in the environment, use this default:
-BOARD ?= bluepill
+BOARD ?= nucleo-l073rz
 
 DEFAULT_PERIOD_SENDING ?= 300
 DEFAULT_RESOLUTION ?= 1
@@ -21,12 +21,12 @@ CFLAGS += -DDEFAULT_RESOLUTION=$(DEFAULT_RESOLUTION)
 CFLAGS += -DDEVEUI=\"$(DEVEUI)\" -DAPPEUI=\"$(APPEUI)\" -DAPPKEY=\"$(APPKEY)\"
 CFLAGS += -DDISABLE_LORAMAC_DUTYCYCLE
 
-CFLAGS += "-DSX127X_PARAM_SPI=(SPI_DEV(1))"
-CFLAGS += "-DSX127X_PARAM_SPI_NSS=GPIO_PIN(PORT_B, 12)"
-CFLAGS += "-DSX127X_PARAM_RESET=GPIO_PIN(PORT_A, 8)"
-CFLAGS += "-DSX127X_PARAM_DIO0=GPIO_PIN(PORT_B, 7)"
-CFLAGS += "-DSX127X_PARAM_DIO1=GPIO_PIN(PORT_B, 6)"
-CFLAGS += "-DSX127X_PARAM_DIO2=GPIO_PIN(PORT_B, 5)"
+CFLAGS += "-DSX127X_PARAM_SPI=(SPI_DEV(0))"
+CFLAGS += "-DSX127X_PARAM_SPI_NSS=GPIO_PIN(PORT_A, 4)"
+CFLAGS += "-DSX127X_PARAM_RESET=GPIO_PIN(PORT_C, 9)"
+CFLAGS += "-DSX127X_PARAM_DIO0=GPIO_PIN(PORT_C, 6)"
+CFLAGS += "-DSX127X_PARAM_DIO1=GPIO_PIN(PORT_C, 7)"
+CFLAGS += "-DSX127X_PARAM_DIO2=GPIO_PIN(PORT_C, 8)"
 CFLAGS += "-DSX127X_PARAM_DIO3=GPIO_UNDEF"
 CFLAGS += "-DSX127X_PARAM_PASELECT=(SX127X_PA_BOOST)"
 
@@ -42,18 +42,21 @@ QUIET ?= 1
 USEPKG += semtech-loramac
 USEMODULE += bme280_i2c
 USEMODULE += fmt
+USEMODULE += ho_scaling
 USEMODULE += semtech_loramac_rx
-USEMODULE += stdio_null
 USEMODULE += sx1276
+USEMODULE += xtimer
 
 FEATURES_REQUIRED += periph_adc
 FEATURES_REQUIRED += periph_gpio
 FEATURES_REQUIRED += periph_rtc
 
 # Specify custom dependencies for your application here ...
-APPDEPS = scaling.h
+INCLUDES += -I$(CURDIR)/ho_helper_functions/scaling
+DIRS += $(CURDIR)/ho_helper_functions/scaling
+#APPDEPS = scaling.h
 
 # This has to be the absolute path to the RIOT base directory:
-RIOTBASE ?= $(CURDIR)/../RIOT
+RIOTBASE ?= $(CURDIR)/RIOT
 
 include $(RIOTBASE)/Makefile.include
--- main.c
+++ main.c
@@ -16,13 +16,18 @@
 #include "scaling.h"
 #include "semtech_loramac.h"
 #include "thread.h"
+#include "xtimer.h"
 
 /* Private typedef ---------------------------------------------------*/
 
 /* Private define ----------------------------------------------------*/
 #define SENDER_PRIO (THREAD_PRIORITY_MAIN - 1)
 #define RECV_MSG_QUEUE (4U)
-#define ON_TIME 6
+#define ON_TIME_S 6
+#define PM_LOCK_LEVEL (1)
+
+#define ADC_VREF_INT 7
+#define VREF_INT_CAL ((uint16_t *) ((uint32_t) 0x1FF80078))
 
 #ifndef DEFAULT_PERIOD_SENDING
 #define DEFAULT_PERIOD_SENDING 600
@@ -36,35 +41,38 @@
 /* Private variables -------------------------------------------------*/
 static kernel_pid_t sender_pid;
 static char sender_stack[THREAD_STACKSIZE_MAIN / 2];
-static char _recv_stack[THREAD_STACKSIZE_DEFAULT];
+static char recv_stack[THREAD_STACKSIZE_DEFAULT];
 static uint8_t deveui[LORAMAC_DEVEUI_LEN];
 static uint8_t appeui[LORAMAC_APPEUI_LEN];
 static uint8_t appkey[LORAMAC_APPKEY_LEN];
-static msg_t _recv_queue[RECV_MSG_QUEUE];
+static msg_t recv_queue[RECV_MSG_QUEUE];
 semtech_loramac_t loramac;
 uint8_t message[50] = {0};
-uint32_t interval_sec = DEFAULT_PERIOD_SENDING;
+uint32_t sending_interval_s = DEFAULT_PERIOD_SENDING;
 uint8_t resolution = DEFAULT_RESOLUTION;
 bmx280_t dev;
 bool bmx280_initialized = false;
 
 /* Private function prototypes ---------------------------------------*/
 static void rtc_cb(void *arg);
-static int _prepare_next_alarm(void);
-static void _send_message(uint8_t length);
+static int prepare_next_alarm(void);
+static void send_message(uint8_t length);
 static void *sender(void *arg);
-static void *_recv(void *arg);
-float _get_vcc(void);
-void _init_unused_pins(void);
+static void *recv(void *arg);
+float get_vcc(void);
+void init_unused_pins(void);
+static void set_resolution(uint8_t received_resolution);
+static void set_sending_interval(uint16_t received_interval);
 
 /* Private functions -------------------------------------------------*/
-int main(void) {
+int main(void)
+{
     uint8_t number_join_tries = 0;
     uint8_t loramac_datarate = 0;
 
     /* Init peripherie */
-    _init_unused_pins();
-    adc_init(ADC_LINE(9));
+    init_unused_pins();
+    adc_init(ADC_LINE(ADC_VREF_INT));
 
     switch (bmx280_init(&dev, &bmx280_params[0])) {
         case BMX280_ERR_BUS:
@@ -106,7 +114,6 @@ int main(void) {
      * keys.
      */
     puts("Starting join procedure");
-    LED0_ON;
     while (semtech_loramac_join(&loramac, LORAMAC_JOIN_OTAA) != SEMTECH_LORAMAC_JOIN_SUCCEEDED) {
         puts("Join procedure failed, try in 30s again");
         xtimer_sleep(22);
@@ -121,13 +128,12 @@ int main(void) {
         }
     }
     puts("Join procedure succeeded");
-    LED0_OFF;
 
     /* start the sender thread */
     sender_pid = thread_create(sender_stack, sizeof(sender_stack), SENDER_PRIO, 0, sender, NULL, "sender");
 
     /* start the receive thread */
-    thread_create(_recv_stack, sizeof(_recv_stack), THREAD_PRIORITY_MAIN - 1, 0, _recv, NULL, "recv thread");
+    thread_create(recv_stack, sizeof(recv_stack), THREAD_PRIORITY_MAIN - 1, 0, recv, NULL, "recv thread");
 
     /* trigger the first send */
     msg_t msg;
@@ -135,26 +141,35 @@ int main(void) {
     return 0;
 }
 
-static void rtc_cb(void *arg) {
+static void rtc_cb(void *arg)
+{
     (void)arg;
+    
+    /* block sleep level mode until the next sending cycle has completed */
+    pm_block(PM_LOCK_LEVEL);
+
     msg_t msg;
+    //disable_gpio_irq();
     msg_send(&msg, sender_pid);
 }
 
-static int _prepare_next_alarm(void) {
+static int prepare_next_alarm(void)
+{
     struct tm time;
-    struct tm alarm;
+    struct tm get_alarm;
+    struct tm set_alarm;
     int rc;
     uint8_t tries = 3;
 
     rtc_get_time(&time);
-    time.tm_sec += (interval_sec - ON_TIME);
-    mktime(&time);
+    memcpy(&set_alarm, &time, sizeof(struct tm));
+    set_alarm.tm_sec += (sending_interval_s - ON_TIME_S);
+    mktime(&set_alarm);
 
     do {
-        rtc_set_alarm(&time, rtc_cb, NULL);
-        rtc_get_alarm(&alarm);
-        rc = rtc_tm_compare(&time, &alarm);
+        rtc_set_alarm(&set_alarm, rtc_cb, NULL);
+        rtc_get_alarm(&get_alarm);
+        rc = rtc_tm_compare(&set_alarm, &get_alarm);
         tries--;
     } while ((rc != 0) && (tries != 0));
 
@@ -167,8 +182,11 @@ static int _prepare_next_alarm(void) {
     return rc;
 }
 
-static void _send_message(uint8_t length) {
-    puts("Sending data");
+static void send_message(uint8_t length)
+{
+    //TODO print data and port
+
+    printf("Sending\n");
     /* Try to send the message */
     uint8_t ret = semtech_loramac_send(&loramac, message, length);
     if (ret != SEMTECH_LORAMAC_TX_DONE) {
@@ -211,12 +229,12 @@ static void *sender(void *arg) {
         if (sensor_status != 0) {
             semtech_loramac_set_tx_port(&loramac, 10);  //TODO Port enum
             message[0] = sensor_status;
-            _send_message(1);
+            send_message(1);
         } else {
             if (resolution == 0) {
                 semtech_loramac_set_tx_port(&loramac, 1);  //TODO Port enum
 
-                uint8_t vbat = (uint8_t)scaling_float(_get_vcc(), 2.0, 4.0, 0.0, 255.0, LIMIT_OUTPUT);
+                uint8_t vbat = (uint8_t)scaling_float(get_vcc(), 2.0, 4.0, 0.0, 255.0, LIMIT_OUTPUT);
                 uint8_t temperature = (uint8_t)scaling_float(temperature_f, -4000.0, 8500.0, 0.0, 250.0, LIMIT_OUTPUT);
                 uint8_t humidity = (uint8_t)scaling_float(humidity_f, 0.0, 10000.0, 0.0, 200.0, LIMIT_OUTPUT);
                 uint8_t pressure = (uint8_t)scaling_float(pressure_f, 95000.0, 105000.0, 0.0, 200.0, LIMIT_OUTPUT);
@@ -226,11 +244,11 @@ static void *sender(void *arg) {
                 message[2] = (uint8_t)humidity;
                 message[3] = (uint8_t)pressure;
 
-                _send_message(4);
+                send_message(4);
             } else if (resolution == 1) {
                 semtech_loramac_set_tx_port(&loramac, 2);  //TODO Port enum
 
-                uint16_t vbat = (uint16_t)scaling_float(_get_vcc(), 2.0, 4.0, 0.0, 65535.0, LIMIT_OUTPUT);
+                uint16_t vbat = (uint16_t)scaling_float(get_vcc(), 2.0, 4.0, 0.0, 65535.0, LIMIT_OUTPUT);
                 uint16_t temperature = (uint16_t)scaling_float(temperature_f, -4000.0, 8500.0, 0.0, 65535.0, LIMIT_OUTPUT);
                 uint16_t humidity = (uint16_t)scaling_float(humidity_f, 0.0, 10000.0, 0.0, 65535.0, LIMIT_OUTPUT);
                 uint16_t pressure = (uint16_t)scaling_float(pressure_f, 95000.0, 105000.0, 0.0, 65535.0, LIMIT_OUTPUT);
@@ -244,18 +262,19 @@ static void *sender(void *arg) {
                 message[6] = (uint8_t)(pressure >> 8);
                 message[7] = (uint8_t)(pressure);
 
-                _send_message(8);
+                send_message(8);
             }
         }
 
-        /* wait some time to give receive thread time to work */
-        xtimer_sleep(1);
-
         /* Schedule the next wake-up alarm */
-        if (_prepare_next_alarm() == 0) {
+        int rc_rtc = 0;
+
+        rc_rtc = prepare_next_alarm();
+
+        if (rc_rtc == 0) {
             /* going to deep sleep */
             puts("Going to sleep");
-            pm_set(1);
+            pm_unblock(PM_LOCK_LEVEL);
         } else {
             /* rtc can not set, try reboot */
             pm_reboot();
@@ -266,91 +285,128 @@ static void *sender(void *arg) {
     return NULL;
 }
 
-static void *_recv(void *arg) {
-    msg_init_queue(_recv_queue, RECV_MSG_QUEUE);
+static void *recv(void *arg)
+{
+    msg_init_queue(recv_queue, RECV_MSG_QUEUE);
 
     (void)arg;
-    uint16_t received_interval = 0;
-    uint8_t received_resolution = 0;
 
     while (1) {
         /* blocks until some data is received */
-        semtech_loramac_recv(&loramac);
-        loramac.rx_data.payload[loramac.rx_data.payload_len] = 0;
-        puts("Data received:");
-        for (uint8_t i = 0; i < loramac.rx_data.payload_len; i++) {
-            print_byte_hex(loramac.rx_data.payload[i]);
-        }
-        puts("\nPort:");
-        print_u32_dec((uint32_t)loramac.rx_data.port);
-        puts("");
+        switch (semtech_loramac_recv(&loramac)) {
+            case SEMTECH_LORAMAC_RX_DATA:
+                loramac.rx_data.payload[loramac.rx_data.payload_len] = 0;
+                printf("Data received: %s, port: %d\n", (char *)loramac.rx_data.payload, loramac.rx_data.port);
 
-        /* process received data */
-        switch (loramac.rx_data.port) {
-            case 1: /* resolution */
-                received_resolution = loramac.rx_data.payload[0];
-                if (received_resolution <= 1) {
-                    resolution = received_resolution;
+                /* process received data */
+                switch (loramac.rx_data.port) {
+                    case 1:
+                        set_resolution(loramac.rx_data.payload[0]);
+                        break;
+                    
+                    case 2:
+                        set_sending_interval((loramac.rx_data.payload[0] << 8) + loramac.rx_data.payload[1]);
+                        break;
+                    
+                    case 3: /* System reboot */
+                        if (loramac.rx_data.payload[0] == 1) {
+                            pm_reboot();
+                        }
+                        break;
+                    
+                    default:
+                        break;
                 }
                 break;
-            case 2: /* time of period sending */
-                received_interval = (loramac.rx_data.payload[0] << 8) + loramac.rx_data.payload[1];
-                if (received_interval >= 3) {
-                    interval_sec = received_interval * 10;
-                }
+
+            case SEMTECH_LORAMAC_RX_CONFIRMED:
+                puts("Received ACK from network");
                 break;
-            case 3: /* System reboot */
-                if (loramac.rx_data.payload[0] == 1) {
-                    pm_reboot();
-                }
+
+            default:
                 break;
         }
     }
     return NULL;
 }
 
-float _get_vcc(void) {
-    float vbat = 4957.744 / adc_sample(ADC_LINE(9), ADC_RES_12BIT);
+float get_vcc(void)
+{
+    uint16_t *vref_int_cal = VREF_INT_CAL;
+    float vbat = 3.0 * *vref_int_cal / adc_sample(ADC_VREF_INT, ADC_RES_12BIT); //TODO catch failure of adc_sample 
     return vbat;
 }
 
-void _init_unused_pins(void) {
+void init_unused_pins(void)
+{
     gpio_t unused_pins[] = {
         GPIO_PIN(PORT_A, 0),
         GPIO_PIN(PORT_A, 1),
         GPIO_PIN(PORT_A, 2),
         GPIO_PIN(PORT_A, 3),
-        GPIO_PIN(PORT_A, 4),
-        GPIO_PIN(PORT_A, 5),
-        GPIO_PIN(PORT_A, 6),
-        GPIO_PIN(PORT_A, 7),
-        //GPIO_PIN(PORT_A, 8),MCO
+        //GPIO_PIN(PORT_A, 4), NSS
+        //GPIO_PIN(PORT_A, 5), SCK
+        //GPIO_PIN(PORT_A, 6), MISO
+        //GPIO_PIN(PORT_A, 7), MOSI
+        GPIO_PIN(PORT_A, 8),
         GPIO_PIN(PORT_A, 9),
         GPIO_PIN(PORT_A, 10),
         GPIO_PIN(PORT_A, 11),
-        //GPIO_PIN(PORT_A, 12), USB pullup
+        GPIO_PIN(PORT_A, 12),
         GPIO_PIN(PORT_A, 13),
         GPIO_PIN(PORT_A, 14),
         GPIO_PIN(PORT_A, 15),
         GPIO_PIN(PORT_B, 0),
         GPIO_PIN(PORT_B, 1),
+        GPIO_PIN(PORT_B, 2),
         GPIO_PIN(PORT_B, 3),
         GPIO_PIN(PORT_B, 4),
-        //GPIO_PIN(PORT_B, 5), DIO2
-        //GPIO_PIN(PORT_B, 6), DIO1
-        //GPIO_PIN(PORT_B, 7), DIO0
-        //GPIO_PIN(PORT_B, 8), SDA
-        //GPIO_PIN(PORT_B, 9), SCK
+        GPIO_PIN(PORT_B, 5),
+        GPIO_PIN(PORT_B, 6),
+        GPIO_PIN(PORT_B, 7),
+        //GPIO_PIN(PORT_B, 8), SCL
+        //GPIO_PIN(PORT_B, 9), SDA
         GPIO_PIN(PORT_B, 10),
         GPIO_PIN(PORT_B, 11),
-        //GPIO_PIN(PORT_B, 12), NNS
-        //GPIO_PIN(PORT_B, 13), SCK
-        //GPIO_PIN(PORT_B, 14), MISO
-        //GPIO_PIN(PORT_B, 15), MOSI
-        //GPIO_PIN(PORT_C, 13), LED0
+        GPIO_PIN(PORT_B, 12),
+        GPIO_PIN(PORT_B, 13),
+        GPIO_PIN(PORT_B, 14),
+        GPIO_PIN(PORT_B, 15),
+        GPIO_PIN(PORT_C, 0),
+        GPIO_PIN(PORT_C, 1),
+        GPIO_PIN(PORT_C, 2),
+        GPIO_PIN(PORT_C, 3),
+        GPIO_PIN(PORT_C, 4),
+        GPIO_PIN(PORT_C, 5),
+        //GPIO_PIN(PORT_C, 6), DIO0
+        //GPIO_PIN(PORT_C, 7), DIO1
+        //GPIO_PIN(PORT_C, 8), DIO2
+        //GPIO_PIN(PORT_C, 9), RESET
+        GPIO_PIN(PORT_C, 10),
+        GPIO_PIN(PORT_C, 11),
+        GPIO_PIN(PORT_C, 12),
+        GPIO_PIN(PORT_C, 13),
+        //GPIO_PIN(PORT_C, 14), RTC crystal
+        //GPIO_PIN(PORT_C, 15), RTC crystal
+        //GPIO_PIN(PORT_D, 2), MOSFET
+        GPIO_PIN(PORT_H, 0),
+        GPIO_PIN(PORT_H, 1)
     };
-
     for (uint8_t i = 0; i < (sizeof(unused_pins) / sizeof(gpio_t)); i++) {
         gpio_init(unused_pins[i], GPIO_IN_PD);
+    }
+}
+
+static void set_resolution(uint8_t received_resolution)
+{
+    if (received_resolution <= 1) {
+        resolution = received_resolution;
+    }
+}
+
+static void set_sending_interval(uint16_t received_interval)
+{
+    if (received_interval >= 3) {
+        sending_interval_s = received_interval * 10;
     }
 }
